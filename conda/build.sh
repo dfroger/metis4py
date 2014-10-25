98aa04d90da0d22ad9171b5340ca074c231925de
David Froger <david.froger@gmail.com>
2014-10-25 08:59:52 +0200

    Use embedded waf in conda script

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,7 @@
-waf configure --metis-inc=$PREFIX/include/ \
-              --metis-lib=$PREFIX/lib/ \
-              --numpy-inc=$PREFIX/lib/python2.7/site-packages/numpy/core/include \
-              --python-inc=$PREFIX/include/python2.7 \
-              --prefix=$PREFIX
-waf build
-waf install
+./waf configure --metis-inc=$PREFIX/include/ \
+                --metis-lib=$PREFIX/lib/ \
+                --numpy-inc=$PREFIX/lib/python2.7/site-packages/numpy/core/include \
+                --python-inc=$PREFIX/include/python2.7 \
+                --prefix=$PREFIX
+./waf build
+./waf install
